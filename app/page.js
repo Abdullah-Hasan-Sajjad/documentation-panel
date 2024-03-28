b3878c615ea40cbb54ca05fb6553dc123bc2458e
Abdullah Hasan Sajjad Rafi <rafiabdullahhasansajjad@gmail.com>
2024-03-28 14:49:03 +0600

    made footer and sidebar responsive

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="bg-red-300 flex min-h-screen flex-col items-center justify-between p-24 md:ml-64">
     <div>
-      Hello
+      Root url
     </div>
     </main>
   );
